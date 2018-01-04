12175ef3b247df1910bad4695a74a8bdce27f1f4
YoonJeongbu <coma333ryu@gmail.com>
2018-01-05 00:40:01 +0900

    vue-cli를 이용한 프로젝트 생성, style scoped, global과 local component에 대한 정리

---
+++ basic7/vuejs-playlist/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <!-- <p>{{greeting()}}</p> -->
+    <comas></comas>
+  </div>
+</template>
+
+<script>
+//Local Component
+import Comas from "./Coma.vue";
+
+export default {
+  components: {
+    comas: Comas
+  },
+  data() {
+    return {
+      title: "Coma App"
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* 
+같은 스타일이 존재하면 마지막 스타일만 적용됨 
+스타일 스코프를 주기위해 scoped사용
+*/
+h1 {
+  color: red;
+}
+</style>
---
+++ basic7/vuejs-playlist/src/Coma.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <h1>Comas List</h1> Vue 템플릿은 하나의 루트 엘리먼트를 가질수 있으므로 오류남-->
+  <div>
+    <h1>Comas List</h1>
+    <ul>
+        <li v-for="coma in comas">{{coma}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      comas: ["coma333", "coma333ryu", "coma-y"]
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
