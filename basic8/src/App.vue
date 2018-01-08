3a0e01d66c919b695bed14a6284babe06f446584
YoonJeongbu <coma333ryu@gmail.com>
2018-01-09 00:45:01 +0900

    basic8 added

---
+++ basic8/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-coma></app-coma>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Coma from './components/Coma'
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-coma': Coma
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ basic8/src/components/Coma.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="comas">
+      <ul>
+          <li v-for="coma in comas" v-on:click="coma.show = !coma.show">
+              <h2>{{coma.name}}</h2>
+              <h3 v-show="coma.show">{{coma.hobby}}</h3>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      comas: [
+          {name:'coma333', hobby:'game', show: false},
+          {name:'coma', hobby:'boxing', show: false},
+          {name:'coma333ryu', hobby:'sleeping', show: false}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#comas{
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+ul{
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+}
+li{
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    margin: 10px;
+}
+</style>
